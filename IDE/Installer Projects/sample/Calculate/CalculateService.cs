eda18bbea74a5344a4125d0382791b5f8418a4dd
NBE <newbigwater@gmail.com>
2024-04-03 10:24:05 +0900

    [IDE][VS2019] installer projects

---
+++ IDE/Installer Projects/sample/Calculate/CalculateService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculate
+{
+    public static class CalculateService
+    {
+        public static double Add(double n1, double n2)
+        {
+            double result = n1 + n2;
+            Console.WriteLine("Received Add({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public static double Subtract(double n1, double n2)
+        {
+            double result = n1 - n2;
+            Console.WriteLine("Received Subtract({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public static double Multiply(double n1, double n2)
+        {
+            double result = n1 * n2;
+            Console.WriteLine("Received Multiply({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public static double Divide(double n1, double n2)
+        {
+            double result = n1 / n2;
+            Console.WriteLine("Received Divide({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+    }
+}
---
+++ IDE/Installer Projects/sample/Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Calculate;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = CalculateService.Add(1, 2);
+            Console.WriteLine($"Add(1, 2) = {result}\n");
+
+            result = CalculateService.Subtract(1, 2);
+            Console.WriteLine($"Subtract(1, 2) = {result}\n");
+
+            result = CalculateService.Multiply(1, 2);
+            Console.WriteLine($"Multiply(1, 2) = {result}\n");
+
+            result = CalculateService.Divide(1, 2);
+            Console.WriteLine($"Divide(1, 2) = {result}\n");
+        }
+    }
+}
