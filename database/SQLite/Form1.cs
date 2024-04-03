94236bc5b59ff3ae88f4a8ee2cfc7daed5a64268
NBE <newbigwater@gmail.com>
2024-04-03 10:31:08 +0900

    [database] SQLite

---
+++ database/SQLite/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string strConnection = @"Data Source=./haksa";
+            SQLiteConnection conn = new SQLiteConnection(strConnection);
+            conn.Open();
+
+            string SQL = "SELECT * FROM student";
+            SQLiteCommand cmd = new SQLiteCommand(SQL, conn);
+            SQLiteDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                listBox1.Items.Add(dr["id"].ToString() + "\t" +
+                                   dr["name"].ToString() + "\t" +
+                                   dr["age"].ToString() + "\t" +
+                                   dr["phone"].ToString());
+            }
+            dr.Close();
+            conn.Close();
+        }
+    }
+}
---
+++ database/SQLite/SQLiteMgr.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLite
+{
+
+
+    /*
+     * [NuGet Package 추가하기]
+
+
+     * */
+    /// <summary>
+    /// 도구 -> NuGet 패키지 관리자 -> 패키지 관리자 콘솔
+    /// <br/>Install-Package System.Data.SQLite 입력하여 설치
+    /// </summary>
+    public class SQLiteMgr
+    {
+        private static string dataSource = @"Data Source=./haksa";
+        SQLiteDataAdapter adpt;
+
+        public DataSet SelectAll(string table)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+
+                string sql = $"SELECT * FROM {table}";
+                adpt = new SQLiteDataAdapter(sql, dataSource);
+                adpt.Fill(ds, table);
+
+                if (ds.Tables.Count > 0) return ds;
+                else return null;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
+        }
+
+        public DataSet SelectDetail(string table, string condition, string where = "")
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+
+                string sql = $"SELECT {condition} FROM {table} {where}";
+                adpt = new SQLiteDataAdapter(sql, dataSource);
+                adpt.Fill(ds, table);
+
+                if (ds.Tables.Count > 0) return ds;
+                else return null;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
+        }
+
+        public void Insert(string table, string value)
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(dataSource))
+                {
+                    conn.Open();
+                    string sql = $"INSERT INTO {table} VALUES ({value})";
+                    SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
+        }
+
+        public void Update(string table, string setvalue, string wherevalue = "")
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(dataSource))
+                {
+                    conn.Open();
+                    string sql = $"UPDATE {table} SET {setvalue} WHERE {wherevalue}";
+                    SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
+        }
+
+        public void DeleteAll(string table)
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(dataSource))
+                {
+                    conn.Open();
+                    string sql = $"DELETE FROM {table}";
+                    SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
+        }
+
+        public void DeleteDetail(string table, string wherecol, string wherevalue)
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(dataSource))
+                {
+                    conn.Open();
+                    string sql = $"DELETE FROM {table} WHERE {wherecol}='{wherevalue}'";
+                    SQLiteCommand cmd = new SQLiteCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                throw;
+            }
+        }
+    }
+}
