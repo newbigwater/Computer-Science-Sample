3445ed8b3c820477f8af6ecabb41c16df6e1e3d9
NBE <newbigwater@gmail.com>
2024-04-03 10:27:10 +0900

    [network] soap

---
+++ network/SOAP/soapSimple/soapClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using soapClient.ServiceReference1;
+
+namespace soapClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SimpleSoapServer Class Name + Client 로 객체 할당 가능
+            var client = new SimpleSoapServerSoapClient();
+
+            List<String> list = client.GetCurrentors("C#");
+            Console.WriteLine($"Count : {client.Count()}");
+            Console.WriteLine($"User List");
+            foreach (var addr in list)
+            {
+                Console.WriteLine($" {addr}");
+            }
+            Console.WriteLine($"[Add] 1 + 2 = {client.Add(1, 2)}");
+            Console.WriteLine($"[Sub] 1 - 2 = {client.Sub(1, 2)}");
+            Console.WriteLine($"[Div] 1 / 2 = {client.Div(1, 2)}");
+            Console.WriteLine($"[Mul] 1 * 2 = {client.Mul(1, 2)}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ network/SOAP/soapSimple/soapServer/SimpleSoapServer.asmx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace soapServer
+{
+    /// <summary>
+    /// Example의 요약 설명입니다.
+    /// </summary>
+    //[WebService(Namespace = "http://tempuri.org/")]
+    [WebService(Namespace = "SoapServer")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // ASP.NET AJAX를 사용하여 스크립트에서 이 웹 서비스를 호출하려면 다음 줄의 주석 처리를 제거합니다. 
+    // [System.Web.Script.Services.ScriptService]
+    public class SimpleSoapServer : System.Web.Services.WebService
+    {
+        // 접속자 address를 넣기 위한 list
+        private static List<String> list = new List<string>();
+
+        // 접속 context, request, response를 받기 위한 맴버 함수
+        private HttpContext context;
+
+        public SimpleSoapServer()
+        {
+            this.context = HttpContext.Current;
+        }
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public int Count()
+        {
+            return list.Count;
+        }
+
+        [WebMethod]
+        public List<String> GetCurrentors(string name)
+        {
+            var address = $@"{this.context.Request.UserHostAddress} ({name})";
+            if (!list.Where(x => x.Equals(address)).Any())
+                list.Add(address);
+            return list;
+        }
+
+        [WebMethod]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        [WebMethod]
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        [WebMethod]
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+
+        [WebMethod]
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
