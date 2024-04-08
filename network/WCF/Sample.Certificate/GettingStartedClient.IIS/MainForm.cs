9bcf34ad0d4048062995410947c27e28753d2438
NBE <newbigwater@gmail.com>
2024-04-08 17:56:06 +0900

    .

---
+++ network/WCF/Sample.Certificate/GettingStartedClient.IIS/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GettingStartedClient.CalculatorService;
+
+namespace GettingStartedClient
+{
+    public partial class MainForm : Form
+    {
+        CalculatorClient client = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            client = new CalculatorClient();
+
+            client.ClientCredentials.ClientCertificate.SetCertificate(
+                System.Security.Cryptography.X509Certificates.StoreLocation.LocalMachine,
+                System.Security.Cryptography.X509Certificates.StoreName.My,
+                System.Security.Cryptography.X509Certificates.X509FindType.FindByThumbprint, "8EF6CDD92C14ABD2F38974CA1F5C1BFDFABA3E24");
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            double result = client.Add(Double.Parse(edt_fir.Text), Double.Parse(edt_sec.Text));
+            lb_result.Text = result.ToString();
+            Console.WriteLine($"Add({edt_fir.Text},{edt_sec.Text}) = {result}");
+        }
+
+        private void btn_sub_Click(object sender, EventArgs e)
+        {
+            double result = client.Subtract(Double.Parse(edt_fir.Text), Double.Parse(edt_sec.Text));
+            lb_result.Text = result.ToString();
+            Console.WriteLine($"Add({edt_fir.Text},{edt_sec.Text}) = {result}");
+        }
+
+        private void btn_mul_Click(object sender, EventArgs e)
+        {
+            double result = client.Multiply(Double.Parse(edt_fir.Text), Double.Parse(edt_sec.Text));
+            lb_result.Text = result.ToString();
+            Console.WriteLine($"Add({edt_fir.Text},{edt_sec.Text}) = {result}");
+        }
+
+        private void btn_div_Click(object sender, EventArgs e)
+        {
+            double result = client.Divide(Double.Parse(edt_fir.Text), Double.Parse(edt_sec.Text));
+            lb_result.Text = result.ToString();
+            Console.WriteLine($"Add({edt_fir.Text},{edt_sec.Text}) = {result}");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            client.Close();
+        }
+    }
+}
