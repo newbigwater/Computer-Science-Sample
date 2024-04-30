83a1836b95e11e96db0e26dc271deb0d0e3f00ea
HRH <cfent12@gmail.com>
2024-04-30 13:24:34 +0900

    Add DisignPattern/Prototype

---
+++ DesignPattern/Prototype/ConcretePrototype.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class ConcretePrototype : IPrototype
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // 복제 생성 메서드
+        public IPrototype Clone()
+        {
+            // 객체를 복제하여 새로운 인스턴스를 반환
+            return new ConcretePrototype { Id = this.Id, Name = this.Name };
+        }
+    }
+}
---
+++ DesignPattern/Prototype/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 원본 프로토타입 객체 생성
+            ConcretePrototype original = new ConcretePrototype { Id = 1, Name = "original" };
+            Console.WriteLine($"original Prototype`s id = {original.Id}, name = {original.Name}");
+            // 원본을 복제하여 새로운 객체 생성
+            ConcretePrototype cloned = (ConcretePrototype)original.Clone();
+            Console.WriteLine($"cloned Prototype`s id = {cloned.Id}, name = {cloned.Name}");
+
+            Console.ReadLine();
+        }
+    }
+}
