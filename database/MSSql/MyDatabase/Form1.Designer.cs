00a4070590c3c7b94f46d75904959b3d9baca9b5
dsshin <dsshin@thirautech.com>
2024-05-10 13:39:10 +0900

    [Database/MSSql] Sample Upload

--- database/MSSql/MyDatabase/Form1.Designer.cs
+++ database/MSSql/MyDatabase/Form1.Designer.cs
@@ -33,31 +33,40 @@ namespace MyDatabase
             this.lb_database = new System.Windows.Forms.Label();
             this.btn_database_isExist = new System.Windows.Forms.Button();
             this.btn_database_create = new System.Windows.Forms.Button();
+            this.btn_table_create = new System.Windows.Forms.Button();
+            this.btn_table_isExist = new System.Windows.Forms.Button();
+            this.lb_table = new System.Windows.Forms.Label();
+            this.edt_table = new System.Windows.Forms.TextBox();
+            this.btn_database_drop = new System.Windows.Forms.Button();
+            this.btn_table_drop = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // edt_database
             // 
-            this.edt_database.Location = new System.Drawing.Point(122, 11);
+            this.edt_database.Location = new System.Drawing.Point(98, 9);
+            this.edt_database.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.edt_database.Name = "edt_database";
-            this.edt_database.Size = new System.Drawing.Size(200, 28);
+            this.edt_database.Size = new System.Drawing.Size(161, 25);
             this.edt_database.TabIndex = 0;
             this.edt_database.Text = "MyDatabase";
             this.edt_database.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // lb_database
             // 
-            this.lb_database.Location = new System.Drawing.Point(12, 9);
+            this.lb_database.Location = new System.Drawing.Point(10, 7);
+            this.lb_database.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lb_database.Name = "lb_database";
-            this.lb_database.Size = new System.Drawing.Size(105, 30);
+            this.lb_database.Size = new System.Drawing.Size(84, 25);
             this.lb_database.TabIndex = 1;
             this.lb_database.Text = "Database : ";
             this.lb_database.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // btn_database_isExist
             // 
-            this.btn_database_isExist.Location = new System.Drawing.Point(332, 11);
+            this.btn_database_isExist.Location = new System.Drawing.Point(266, 9);
+            this.btn_database_isExist.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_database_isExist.Name = "btn_database_isExist";
-            this.btn_database_isExist.Size = new System.Drawing.Size(100, 30);
+            this.btn_database_isExist.Size = new System.Drawing.Size(80, 25);
             this.btn_database_isExist.TabIndex = 2;
             this.btn_database_isExist.Text = "Is Exist?";
             this.btn_database_isExist.UseVisualStyleBackColor = true;
@@ -65,23 +74,95 @@ namespace MyDatabase
             // 
             // btn_database_create
             // 
-            this.btn_database_create.Location = new System.Drawing.Point(437, 11);
+            this.btn_database_create.Location = new System.Drawing.Point(350, 9);
+            this.btn_database_create.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_database_create.Name = "btn_database_create";
-            this.btn_database_create.Size = new System.Drawing.Size(100, 30);
+            this.btn_database_create.Size = new System.Drawing.Size(80, 25);
             this.btn_database_create.TabIndex = 3;
             this.btn_database_create.Text = "Create";
             this.btn_database_create.UseVisualStyleBackColor = true;
             this.btn_database_create.Click += new System.EventHandler(this.btn_database_create_Click);
             // 
+            // btn_table_create
+            // 
+            this.btn_table_create.Location = new System.Drawing.Point(350, 48);
+            this.btn_table_create.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_table_create.Name = "btn_table_create";
+            this.btn_table_create.Size = new System.Drawing.Size(80, 25);
+            this.btn_table_create.TabIndex = 7;
+            this.btn_table_create.Text = "Create";
+            this.btn_table_create.UseVisualStyleBackColor = true;
+            this.btn_table_create.Click += new System.EventHandler(this.btn_table_create_Click);
+            // 
+            // btn_table_isExist
+            // 
+            this.btn_table_isExist.Location = new System.Drawing.Point(266, 48);
+            this.btn_table_isExist.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_table_isExist.Name = "btn_table_isExist";
+            this.btn_table_isExist.Size = new System.Drawing.Size(80, 25);
+            this.btn_table_isExist.TabIndex = 6;
+            this.btn_table_isExist.Text = "Is Exist?";
+            this.btn_table_isExist.UseVisualStyleBackColor = true;
+            this.btn_table_isExist.Click += new System.EventHandler(this.btn_table_isExist_Click);
+            // 
+            // lb_table
+            // 
+            this.lb_table.Location = new System.Drawing.Point(10, 46);
+            this.lb_table.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lb_table.Name = "lb_table";
+            this.lb_table.Size = new System.Drawing.Size(84, 25);
+            this.lb_table.TabIndex = 5;
+            this.lb_table.Text = "Table : ";
+            this.lb_table.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // edt_table
+            // 
+            this.edt_table.Location = new System.Drawing.Point(98, 48);
+            this.edt_table.Margin = new System.Windows.Forms.Padding(2);
+            this.edt_table.Name = "edt_table";
+            this.edt_table.Size = new System.Drawing.Size(161, 25);
+            this.edt_table.TabIndex = 4;
+            this.edt_table.Text = "MyTable";
+            this.edt_table.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_database_drop
+            // 
+            this.btn_database_drop.Location = new System.Drawing.Point(434, 10);
+            this.btn_database_drop.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_database_drop.Name = "btn_database_drop";
+            this.btn_database_drop.Size = new System.Drawing.Size(80, 25);
+            this.btn_database_drop.TabIndex = 8;
+            this.btn_database_drop.Text = "Drop";
+            this.btn_database_drop.UseVisualStyleBackColor = true;
+            this.btn_database_drop.Click += new System.EventHandler(this.btn_database_drop_Click);
+            // 
+            // btn_table_drop
+            // 
+            this.btn_table_drop.Location = new System.Drawing.Point(434, 48);
+            this.btn_table_drop.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_table_drop.Name = "btn_table_drop";
+            this.btn_table_drop.Size = new System.Drawing.Size(80, 25);
+            this.btn_table_drop.TabIndex = 9;
+            this.btn_table_drop.Text = "Drop";
+            this.btn_table_drop.UseVisualStyleBackColor = true;
+            this.btn_table_drop.Click += new System.EventHandler(this.btn_table_drop_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(640, 375);
+            this.Controls.Add(this.btn_table_drop);
+            this.Controls.Add(this.btn_database_drop);
+            this.Controls.Add(this.btn_table_create);
+            this.Controls.Add(this.btn_table_isExist);
+            this.Controls.Add(this.lb_table);
+            this.Controls.Add(this.edt_table);
             this.Controls.Add(this.btn_database_create);
             this.Controls.Add(this.btn_database_isExist);
             this.Controls.Add(this.lb_database);
             this.Controls.Add(this.edt_database);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -95,6 +176,12 @@ namespace MyDatabase
         private System.Windows.Forms.Label lb_database;
         private System.Windows.Forms.Button btn_database_isExist;
         private System.Windows.Forms.Button btn_database_create;
+        private System.Windows.Forms.Button btn_table_create;
+        private System.Windows.Forms.Button btn_table_isExist;
+        private System.Windows.Forms.Label lb_table;
+        private System.Windows.Forms.TextBox edt_table;
+        private System.Windows.Forms.Button btn_database_drop;
+        private System.Windows.Forms.Button btn_table_drop;
     }
 }
 
--- database/MSSql/MyDatabase/Form1.cs
+++ database/MSSql/MyDatabase/Form1.cs
@@ -21,16 +21,242 @@ namespace MyDatabase
             InitializeComponent();
         }
 
+        public bool IsExistDatabase(string databaseName)
+        {
+            string masterConnInfo = $"" +
+                                    $"Integrated Security=SSPI;" +
+                                    $"connection timeout=30;" +
+                                    $"server={ConfigurationManager.AppSettings["IP"]}, {ConfigurationManager.AppSettings["PORT"]};";
+            bool isExisted = false;
+            SqlConnection masterConn = new SqlConnection(masterConnInfo);
+            masterConn.Open();
+
+            //Database
+            {
+                //                 string query = string.Format($"select * from sys.Sysdatabases " + $"where name='{edt_database.Text}';");
+                //                 SqlCommand masterCmd = new SqlCommand(query, masterConn);
+                //                 {
+                //                     SqlDataReader reader = masterCmd.ExecuteReader();
+                //                     {
+                //                         isExisted = reader.HasRows;
+                //                     }
+                //                     reader.Close();
+                //                     reader.Dispose();
+                //                 }
+
+                //                 if (isExisted)
+                //                     MessageBox.Show($"{edt_database.Text} Database가 존재합니다.", "Is Exist?", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //                 else
+                //                     MessageBox.Show($"{edt_database.Text} Database가 존재하지 않습니다.", "Is Exist?", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                string query = $"IF EXISTS (SELECT * FROM sys.databases WHERE name = '{databaseName}') SELECT 1 ELSE SELECT 0";
+                using (SqlCommand command = new SqlCommand(query, masterConn))
+                {
+                    int result = (int)command.ExecuteScalar();
+
+                    if (result == 1)
+                        return true;
+                    else
+                        return false;
+                }
+            }
+        }
+
         private void btn_database_isExist_Click(object sender, EventArgs e)
         {
-            SqlConnection sqlMasterConn = null;
-            string connstr = "SERVER=" + ConfigurationManager.AppSettings["IP"] + "," + ConfigurationManager.AppSettings["PORT"] + ";" +
-                             "DATABASE=" + ConfigurationManager.AppSettings["DBNAME"] + ";UID=" + ConfigurationManager.AppSettings["USERID"] + ";PASSWORD=" + ConfigurationManager.AppSettings["USERPASSWORD"];
+            if(IsExistDatabase(edt_database.Text))
+                MessageBox.Show($"{edt_database.Text} Database가 존재합니다.", "Is Exist?", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show($"{edt_database.Text} Database가 존재하지 않습니다.", "Is Exist?", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
         }
 
         private void btn_database_create_Click(object sender, EventArgs e)
         {
+            string databaseName = $"{edt_database.Text}";  // 확인하고자 하는 데이터베이스 이름
 
+            if (IsExistDatabase(databaseName))
+            {
+                MessageBox.Show($"{databaseName} Database가 이미 존재합니다.", "Is Exist?", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string masterConnInfo = $"" +
+                                    $"Integrated Security=SSPI;" +
+                                    $"connection timeout=30;" +
+                                    $"server={ConfigurationManager.AppSettings["IP"]}, {ConfigurationManager.AppSettings["PORT"]};";
+            using (SqlConnection connection = new SqlConnection(masterConnInfo))
+            {
+                connection.Open();
+
+                string query = $"CREATE DATABASE [{databaseName}];";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            if (IsExistDatabase(databaseName))
+                MessageBox.Show($"{databaseName} Database가 생성되었습니다.", "Create", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show($"{databaseName} Database가 생성되지 못했습니다.", "Create", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void btn_database_drop_Click(object sender, EventArgs e)
+        {
+            string databaseName = $"{edt_database.Text}";  // 확인하고자 하는 데이터베이스 이름
+
+            if (!IsExistDatabase(databaseName))
+            {
+                MessageBox.Show($"{databaseName} Database가 존재하지 않습니다.", "Is Exist?", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string masterConnInfo = $"" +
+                                    $"Integrated Security=SSPI;" +
+                                    $"connection timeout=30;" +
+                                    $"server={ConfigurationManager.AppSettings["IP"]}, {ConfigurationManager.AppSettings["PORT"]};";
+            using (SqlConnection connection = new SqlConnection(masterConnInfo))
+            {
+                connection.Open();
+                
+                string query = $@"
+                IF DB_ID(N'{databaseName}') IS NOT NULL
+                BEGIN
+                    ALTER DATABASE [{databaseName}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+                    DROP DATABASE [{databaseName}];
+                END";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            if (IsExistDatabase(databaseName))
+                MessageBox.Show($"{databaseName} Database가 삭제되지 않았습니다.", "Drop", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+                MessageBox.Show($"{databaseName} Database가 삭제되었습니다.", "Drop", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public bool IsExistTable(string databaseName, string tableName)
+        {
+            string connInfo = $"" +
+                                    $"Integrated Security=False;" +
+                                    $"connection timeout=30;" +
+                                    $"server={ConfigurationManager.AppSettings["IP"]}, {ConfigurationManager.AppSettings["PORT"]};" +
+                                    $"database={databaseName};" +
+                                    $"user id={ConfigurationManager.AppSettings["USERID"]};" +
+                                    $"password={ConfigurationManager.AppSettings["USERPASSWORD"]};";
+            SqlConnection masterConn = new SqlConnection(connInfo);
+            masterConn.Open();
+
+            //Database
+            {
+                string query = $"IF EXISTS (SELECT * FROM sys.databases WHERE name = '{databaseName}') SELECT 1 ELSE SELECT 0";
+                using (SqlCommand command = new SqlCommand(query, masterConn))
+                {
+                    int result = (int)command.ExecuteScalar();
+
+                    if (result == 0)
+                    {
+                        MessageBox.Show($"{databaseName} Database가 존재하지 않습니다.", "Is Exist?", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return false;
+                    }
+                }
+            }
+
+            //Table
+            {
+                string query = $"IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = '{tableName}') " +
+                               $"SELECT 1 ELSE SELECT 0";
+                using (SqlCommand command = new SqlCommand(query, masterConn))
+                {
+                    int result = (int)command.ExecuteScalar();
+
+                    if (result == 1)
+                        return true;
+                    else
+                        return false;
+                }
+            }
+        }
+
+        private void btn_table_isExist_Click(object sender, EventArgs e)
+        {
+            if(IsExistTable(edt_database.Text, edt_table.Text))
+                MessageBox.Show($"{edt_table.Text} Table이 존재합니다.", "Is Exist?", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show($"{edt_table.Text} Table이 존재하지 않습니다.", "Is Exist?", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void btn_table_create_Click(object sender, EventArgs e)
+        {
+            string databaseName = $"{edt_database.Text}";
+            string tableName = $"{edt_table.Text}";
+
+            if (IsExistTable(databaseName, tableName))
+            {
+                MessageBox.Show($"{tableName} Table이 이미 존재합니다.", "Is Exist?", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string connInfo = $"" +
+                              $"Integrated Security=False;" +
+                              $"connection timeout=30;" +
+                              $"server={ConfigurationManager.AppSettings["IP"]}, {ConfigurationManager.AppSettings["PORT"]};" +
+                              $"database={databaseName};" +
+                              $"user id={ConfigurationManager.AppSettings["USERID"]};" +
+                              $"password={ConfigurationManager.AppSettings["USERPASSWORD"]};";
+            using (SqlConnection connection = new SqlConnection(connInfo))
+            {
+                connection.Open();
+                
+                string query = $"CREATE TABLE {tableName} (ID INT PRIMARY KEY IDENTITY(1,1), Name NVARCHAR(100), Age INT);";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            if (IsExistTable(databaseName, tableName))
+                MessageBox.Show($"{tableName} Table이 생성되었습니다.", "Create", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show($"{tableName} Table이 생성되지 못했습니다.", "Create", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void btn_table_drop_Click(object sender, EventArgs e)
+        {
+            string databaseName = $"{edt_database.Text}";
+            string tableName = $"{edt_table.Text}";
+
+            if (!IsExistTable(databaseName, tableName))
+            {
+                MessageBox.Show($"{tableName} Table이 존재하지 않습니다.", "Is Exist?", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string connInfo = $"" +
+                              $"Integrated Security=False;" +
+                              $"connection timeout=30;" +
+                              $"server={ConfigurationManager.AppSettings["IP"]}, {ConfigurationManager.AppSettings["PORT"]};" +
+                              $"database={databaseName};" +
+                              $"user id={ConfigurationManager.AppSettings["USERID"]};" +
+                              $"password={ConfigurationManager.AppSettings["USERPASSWORD"]};";
+            using (SqlConnection connection = new SqlConnection(connInfo))
+            {
+                connection.Open();
+
+                string query = $"DROP TABLE dbo.{tableName};";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            if (IsExistTable(databaseName, tableName))
+                MessageBox.Show($"{tableName} Table이 삭제되지 않았습니다.", "Create", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+                MessageBox.Show($"{tableName} Table이 삭제되었습니다.", "Create", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
